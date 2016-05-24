40a6e09c741d386e50cf2091159ec15befbf8b55
zhangkon <zhangkon@gmail.com>
2016-05-24 12:32:29 +0800

    set navigation bar and status bar

--- Mr-Ride-iOS/HomeViewController.swift
+++ Mr-Ride-iOS/HomeViewController.swift
@@ -11,38 +11,34 @@ import SideMenu
 
 class HomeViewController: UIViewController {
 
-    @IBOutlet weak var totalDistance: UILabel!
     @IBOutlet weak var totalDistanceCount: UILabel!
-    @IBOutlet weak var totalCounts: UILabel!
     @IBOutlet weak var totalCountsCount: UILabel!
-    @IBOutlet weak var averageSpeed: UILabel!
     @IBOutlet weak var averageSpeedCount: UILabel!
     
     @IBOutlet weak var buttonLetsRide: UIButton!
     
-    @IBOutlet weak var logoBike: UIImageView!
-    @IBOutlet weak var buttonBar: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationController?.navigationBar.barTintColor = UIColor.MRLightblueColor()
+        
+        // Set Navigation Bar Item Logo
+        let logo = UIImageView(image: UIImage(named: "logo-bike.png"))
+        logo.image = logo.image!.imageWithRenderingMode(UIImageRenderingMode.AlwaysTemplate)
+        logo.tintColor = .whiteColor()
+        self.navigationItem.titleView = logo
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), forBarMetrics: UIBarMetrics.Default)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+        
+        // Set Let's Ride button
         buttonLetsRide.layer.cornerRadius = 30
         
-        // change buttonBar color
-        let origImage = UIImage(named: "icon-list");
-        let tintedImage = origImage?.imageWithRenderingMode(UIImageRenderingMode.AlwaysTemplate)
-        buttonBar.setImage(tintedImage, forState: .Normal)
-        buttonBar.tintColor = UIColor.whiteColor()
-        
-        // change logoBike color
-        logoBike.image = logoBike.image!.imageWithRenderingMode(UIImageRenderingMode.AlwaysTemplate)
-        logoBike.tintColor = UIColor.whiteColor()
-        
-        //UIBarButtonItem
-        //NSAttributedString
+        //NSAttributedString Use To Count's string interpolation
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
+    
 }
 
