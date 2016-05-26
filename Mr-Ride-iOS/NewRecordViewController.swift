66f5bc10ac41d9a3d4562afafcb01c171afb47a6
zhangkon <zhangkon@gmail.com>
2016-05-26 16:09:30 +0800

    basic stracture

---
+++ Mr-Ride-iOS/NewRecordViewController.swift
@@ -0,0 +1,21 @@
+//
+//  NewRecordViewController.swift
+//  Mr-Ride-iOS
+//
+//  Created by 張瑋康 on 2016/5/24.
+//  Copyright © 2016年 Appworks School Weikz. All rights reserved.
+//
+
+import UIKit
+
+class NewRecordViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
