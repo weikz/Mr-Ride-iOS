d6719b6d865039cf8c19da5cce0424fec9ebec71
zhangkon <zhangkon@gmail.com>
2016-05-28 17:31:01 +0800

    add gradient and google maps sdk

--- Mr-Ride-iOS/NewRecordViewController.swift
+++ Mr-Ride-iOS/NewRecordViewController.swift
@@ -7,15 +7,24 @@
 //
 
 import UIKit
+import GoogleMaps
 
 class NewRecordViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        addGradient()
         
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func addGradient() {
+        let layer = CAGradientLayer()
+        layer.frame = self.view.bounds
+        layer.colors = [UIColor.blackColor().colorWithAlphaComponent(0.6).CGColor, UIColor.blackColor().colorWithAlphaComponent(0.4).CGColor]
+        view.layer.insertSublayer(layer, atIndex: 0)
     }
 }
