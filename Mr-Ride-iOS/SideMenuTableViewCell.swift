324ddf846a43037c8a1f90108d1c82d93a3050be
zhangkon <zhangkon@gmail.com>
2016-06-08 15:19:27 +0800

    add side menu item

---
+++ Mr-Ride-iOS/SideMenuTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  SideMenuTableViewCell.swift
+//  Mr-Ride-iOS
+//
+//  Created by 張瑋康 on 2016/6/8.
+//  Copyright © 2016年 Appworks School Weikz. All rights reserved.
+//
+
+import UIKit
+
+class SideMenuTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var sideMenuItem: UIButton!
+    
+    // MARK: - View Life Cycle
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    
+
+    
+
+}
---
+++ Mr-Ride-iOS/SideMenuTableViewController.swift
@@ -0,0 +1,45 @@
+//
+//  SideMenuTableViewController.swift
+//  Mr-Ride-iOS
+//
+//  Created by 張瑋康 on 2016/6/8.
+//  Copyright © 2016年 Appworks School Weikz. All rights reserved.
+//
+
+import UIKit
+
+class SideMenuTableViewController: UITableViewController {
+
+    let barItems = ["Home", "History", "Map"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.navigationController?.navigationBarHidden = true
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return barItems.count
+    }
+    
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 50
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("sideMenuCell", forIndexPath: indexPath) as! SideMenuTableViewCell
+        cell.sideMenuItem.setTitle(barItems[indexPath.row], forState: .Normal)
+        
+        
+        return cell
+    }
+
+    
+}
